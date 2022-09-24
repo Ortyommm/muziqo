ef61923bfc77fe6560e81a26f42b33f675e023bd
Artem <artemdemargaryan@gmail.com>
2022-09-24 18:14:52 +0300

    bug fix: go to current user page on another user page

--- frontend/src/components/UsersList/UsersPage.tsx
+++ frontend/src/components/UsersList/UsersPage.tsx
@@ -29,7 +29,7 @@ export default function UsersPage() {
       .finally(() => {
         setIsLoading(false);
       });
-  }, []);
+  }, [params.id]);
 
   if (isLoading) return <CircleCenterProgress />;
 
