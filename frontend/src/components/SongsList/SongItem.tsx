9068776be6a8518e9a851122212926a265724a8a
Artem <artemdemargaryan@gmail.com>
2022-10-11 15:53:11 +0300

    trick with audioController for safari

--- frontend/src/components/SongsList/SongItem.tsx
+++ frontend/src/components/SongsList/SongItem.tsx
@@ -112,11 +112,7 @@ export default function SongItem({
           </Box>
         }
       >
-        <IconButton
-          sx={{ mr: 2 }}
-          onClick={onAudioClick}
-          onTouchStart={onAudioClick}
-        >
+        <IconButton sx={{ mr: 2 }} onClick={onAudioClick}>
           {isCurrent && isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
         </IconButton>
         <Box
