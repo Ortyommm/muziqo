479ba4e6353f65ae35487f945edf4701e43a3f0a
Artem <artemdemargaryan@gmail.com>
2022-11-02 20:10:41 +0300

    Update song duration converter

--- frontend/src/utils/durationConverter.ts
+++ frontend/src/utils/durationConverter.ts
@@ -1,5 +1,7 @@
 export function durationConverter(duration: number) {
   const minutes = Math.floor(duration / 60);
-  const seconds = (duration - minutes * 60).toFixed(0).padStart(2, "0");
+  const seconds = Math.floor(duration - minutes * 60)
+    .toString()
+    .padStart(2, "0");
   return `${minutes}:${seconds}`;
 }
